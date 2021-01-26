63b6c13e679aef232a5f3f2eec1c65e55fcf7233
umavictor <umavictor11@gmail.com>
2021-01-26 11:54:57 +0100

    create movie-image component

---
+++ src/components/Movie.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div
+      class="flex flex-col justify-center items-center max-w-sm mx-auto my-8"
+    >
+      <div
+        style="
+            background-image: url(https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80;
+          "
+        class="bg-gray-300 h-64 w-full rounded-lg shadow-md bg-cover bg-center"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -32,23 +32,16 @@
       </div>
     </div>
     <!-- most popular -->
-    <div>
-      <div
-        class="flex flex-col justify-center items-center max-w-sm mx-auto my-8"
-      >
-        <div
-          style="
-            background-image: url(https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80;
-          "
-          class="bg-gray-300 h-64 w-full rounded-lg shadow-md bg-cover bg-center"
-        ></div>
-      </div>
-    </div>
+    <Movie />
   </div>
 </template>
 
 <script>
+import Movie from '@/components/Movie.vue';
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    Movie
+  }
 };
 </script>
