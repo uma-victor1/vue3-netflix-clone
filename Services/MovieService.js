451ad886004a33198a7ba31590eb2f08e83c465e
umavictor <umavictor11@gmail.com>
2021-01-29 11:16:28 +0100

    removed unused code

--- Services/MovieService.js
+++ Services/MovieService.js
@@ -6,17 +6,9 @@ const serviceInstance = axios.create({
   Headers: { Accept: "application/json" },
   timeout: 10000
 });
-const imageServiceInstance = axios.create({
-  baseURL: "https://image.tmdb.org/t/p",
-  Headers: { Accept: "application/json" },
-  timeout: 10000
-});
 
 export default {
   getPopularMovies() {
     return serviceInstance.get(`/movie/popular?api_key=${key.apiKey}`);
-  },
-  getBanner(imgLink) {
-    return imageServiceInstance.get(imgLink);
   }
 };
